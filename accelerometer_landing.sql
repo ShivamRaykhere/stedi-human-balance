0e917939a82fac9b720611a75cafa538c3a37111
SHIVAM RAYKHERE <shivamraykhere3156@gmail.com>
2025-09-25 12:49:10 +0530

    Update accelerometer_landing.sql

--- accelerometer_landing.sql
+++ accelerometer_landing.sql
@@ -1,1 +1,16 @@
-SELECT * FROM accelerometer_landing
+CREATE EXTERNAL TABLE `accelerometer_landing`(
+  `timestamp` bigint COMMENT 'from deserializer', 
+  `user` string COMMENT 'from deserializer', 
+  `x` double COMMENT 'from deserializer', 
+  `y` double COMMENT 'from deserializer', 
+  `z` double COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://mybucketstedi/accelerometer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
