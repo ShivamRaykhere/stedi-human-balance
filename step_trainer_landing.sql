31f9fcf7316640adb64eb6974d0959e60b3bffc4
SHIVAM RAYKHERE <shivamraykhere3156@gmail.com>
2025-09-25 13:18:02 +0530

    Update step_trainer_landing.sql

--- step_trainer_landing.sql
+++ step_trainer_landing.sql
@@ -1,1 +1,14 @@
-SELECT * from step_trainer_landing
+CREATE EXTERNAL TABLE `step_trainer_landing`(
+  `sensorreadingtime` bigint COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `distancefromobject` bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://mybucketstedi/step_trainer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
